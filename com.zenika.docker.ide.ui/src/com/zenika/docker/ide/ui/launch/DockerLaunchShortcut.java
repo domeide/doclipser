1ac250bb1fd9e2e2d9655888c3b4b391fda18461
l0rd <mario.loriedo@gmail.com>
2015-03-16 23:20:18 +0100

    Integrated Docker API

--- com.zenika.docker.ide.ui/src/com/zenika/docker/ide/ui/launch/DockerLaunchShortcut.java
+++ com.zenika.docker.ide.ui/src/com/zenika/docker/ide/ui/launch/DockerLaunchShortcut.java
@@ -1,12 +1,24 @@
 package com.zenika.docker.ide.ui.launch;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.ui.ILaunchShortcut2;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorPart;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
 
 public class DockerLaunchShortcut implements ILaunchShortcut2 {
 
@@ -15,7 +27,48 @@ public class DockerLaunchShortcut implements ILaunchShortcut2 {
 		if (sel instanceof IStructuredSelection) {
 			Object selected = ((IStructuredSelection) sel).getFirstElement();
 			if (selected instanceof IFile) {
-				System.out.println("Selected file: " + ((IFile)selected).getName());
+				IFile dockerfile = (IFile)selected;
+				IPath dockerfilePath = dockerfile.getLocation().removeLastSegments(1);
+				
+				DockerClientConfig config = DockerClientConfig
+						.createDefaultConfigBuilder()
+						.withVersion("1.16")
+						.withUri("https://192.168.59.103:2376")
+						// .withUsername("dockeruser")
+						// .withPassword("ilovedocker")
+						// .withEmail("dockeruser@github.com")
+						// .withServerAddress("https://index.docker.io/v1/")
+						.withDockerCertPath(
+								"/Users/mariolet/.boot2docker/certs/boot2docker-vm")
+						.build();
+				DockerClient dockerClient = DockerClientBuilder.getInstance(config)
+						.build();
+//				Info info = dockerClient.infoCmd().exec();
+//				System.out.print(info);
+				
+				File baseDir = new File(dockerfilePath.toOSString());
+				
+				InputStream response = dockerClient.buildImageCmd(baseDir).exec();
+				
+//				File baseDir = new File("/Users/mariolet/GitHub/sublime-docker/language-helloworlds/dockerfile");
+//
+//				InputStream response = dockerClient.buildImageCmd(baseDir).exec();
+//
+				StringWriter logwriter = new StringWriter();
+
+				try {
+				    LineIterator itr = IOUtils.lineIterator(response, "UTF-8");
+				    while (itr.hasNext()) {
+				        String line = itr.next();
+				        logwriter.write(line);
+				        System.out.println(line);
+				    }
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+				    IOUtils.closeQuietly(response);
+				}
 			}
 		}
 	}
